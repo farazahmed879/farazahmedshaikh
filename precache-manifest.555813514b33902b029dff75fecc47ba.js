0612cc32fe2bb386d1af8612e531fbec34c62a64
Faraz Ahmed Shaikh <farazahmedshaikh879@gmail.com>
2021-07-30 13:42:54 +0500

    Updates

---
+++ precache-manifest.555813514b33902b029dff75fecc47ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fdd7e00a53ff18f98f8ef939fb0c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82c94b5db4903468a215",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "c36dd0e304ee6fd2bf39",
+    "url": "/static/js/2.d76ca910.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d76ca910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c94b5db4903468a215",
+    "url": "/static/js/main.67a84bc8.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "52cf79092195cb11f84917d98fbbafbf",
+    "url": "/static/media/faraz.52cf7909.jpg"
+  }
+]);
